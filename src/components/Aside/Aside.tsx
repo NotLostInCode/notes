2ab46e5440a2862035885c99e4847c4aaf783c90
Gregory <not.lost.in.code@gmail.com>
2023-10-13 19:32:03 +0400

    adding fonts and icons

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import styles from './Aside.module.css'
 import avatar from '../../assets/images/avatar.png'
 
+import settings from '../../assets/icons/setting.svg'
+import arrow from '../../assets/icons/arrow.svg'
+import notes from '../../assets/icons/notes.svg'
+import lists from '../../assets/icons/lists.svg'
+import recording from '../../assets/icons/recording.svg'
+import sketches from '../../assets/icons/sketches.svg'
+import trash from '../../assets/icons/trash.svg'
+
 export const Aside = () => {
     return (
         <aside className={styles.aside}>
@@ -15,10 +23,10 @@ export const Aside = () => {
                 </div>
                 <div className={styles.profileSetting}>
                     <button className={styles.searchDisplay}>
-                        +
+                        <img src={arrow} alt=""/>
                     </button>
                     <button className={styles.setting}>
-                        +
+                        <img src={settings} alt=""/>
                     </button>
                 </div>
             </div>
@@ -35,23 +43,23 @@ export const Aside = () => {
 
             <div className={styles.notesList}>
                 <div className={styles.notes}>
-                    <img className={styles.notesImage} src="" alt=""/>
+                    <img className={styles.notesImage} src={notes} alt=""/>
                     <h3 className={styles.notesName}>Notes</h3>
                 </div>
                 <div className={styles.notes}>
-                    <img className={styles.notesImage} src="" alt=""/>
+                    <img className={styles.notesImage} src={lists} alt=""/>
                     <h3 className={styles.notesName}>Lists</h3>
                 </div>
                 <div className={styles.notes}>
-                    <img className={styles.notesImage} src="" alt=""/>
+                    <img className={styles.notesImage} src={recording} alt=""/>
                     <h3 className={styles.notesName}>Recording</h3>
                 </div>
                 <div className={styles.notes}>
-                    <img className={styles.notesImage} src="" alt=""/>
+                    <img className={styles.notesImage} src={sketches} alt=""/>
                     <h3 className={styles.notesName}>Sketches</h3>
                 </div>
                 <div className={styles.notes}>
-                    <img className={styles.notesImage} src="" alt=""/>
+                    <img className={styles.notesImage} src={trash} alt=""/>
                     <h3 className={styles.notesName}>Trash</h3>
                 </div>
 
